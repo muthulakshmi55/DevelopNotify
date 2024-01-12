1e6f5af0bb6b5fb4a7d8a42eec963deec610d8e1
MuthuLakshmi <muthulakshmi@zoifintech.com>
2024-01-12 17:13:16 +0530

    Testing Send message

--- app.js
+++ app.js
@@ -2,33 +2,15 @@ const dotenv = require('dotenv');
 dotenv.config({path:'./.env'})
 const express = require('express');
 const axios = require('axios');
+const bodyParser = require('body-parser');
+const request = require('request')
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   console.log(process.env.VERIFY_FB_TOKEN)
     res.send('Hello, World!');
-});
-
-app.get('/sendMessengerRequest', (req, res) => {
-    let body = req.body;
-    if (req.method === 'GET') {
-        let VERIFY_TOKEN = process.env.VERIFY_FB_TOKEN
-
-        let mode = req.query['hub.mode'];
-        let token = req.query['hub.verify_token'];
-        let challenge = req.query['hub.challenge'];
-
-        console.log("Token:", token);
-        console.log("Expected Token:", VERIFY_TOKEN);
-        if (mode && token.trim() === VERIFY_TOKEN.trim()) {
-            res.status(200).send(challenge);
-        } else {
-            res.sendStatus(403);
-        }
-
-    } else {
-        res.sendStatus(405); // Method Not Allowed
-    }
 });
 
 app.get('/webhook', (req, res) => {
@@ -51,33 +33,6 @@ app.get('/webhook', (req, res) => {
     }
 });
 
-// app.post('/sendMessengerRequest', async function(req, res) {
-  
-//   try {
-//       let senderId = req.query['senderId'];
-      
-//       await axios.get(`https://graph.facebook.com/v12.0/${senderId}`, {
-//           headers: {
-//               'Accept': 'application/json',
-//               'Accept-Charset': 'utf-8',
-//               'User-Agent': 'test-bot',
-//               'Authorization': `Bearer ${process.env.FB_PAGE_TOKEN}`, // Include the access token in the headers
-//           },
-//           timeout: 5000, // Adjust the timeout as needed
-//       })
-//       .then(response => {
-//           console.log('faceUserInfo', response.data);
-//           res.sendStatus(200);
-//       })
-//       .catch(error => {
-//           console.error('Error fetching user information:', error.message);
-//           res.sendStatus(403);
-//       });
-//   } catch (error) {
-//       console.error('Error sending message:', error.config);
-//       res.sendStatus(404);
-//   }
-// });
 
   app.post('/webhook', (req, res) => {
     let body = req.body;
@@ -111,9 +66,41 @@ app.get('/webhook', (req, res) => {
   function handleMessage(senderPsid, message) {
     const recipientId = senderPsid;
     const messageText = message.text;
-  
+
     console.log('Received message from:', recipientId);
     console.log('Message text:', messageText);
+    if( message && message.attachments && message.attachments[0].payload){
+        callSendAPI(sender_psid, "Thank you for watching my video !!!");
+        callSendAPIWithTemplate(sender_psid);
+        return;
+    }
+
+    let entitiesArr = [ "wit$greetings", "wit$thanks", "wit$bye" ];
+    let entityChosen = "";
+    entitiesArr.forEach((name) => {
+        let entity = firstTrait(message.nlp, name);
+        if (entity && entity.confidence > 0.8) {
+            entityChosen = name;
+        }
+    });
+
+    if(entityChosen === ""){
+        //default
+        callSendAPI(sender_psid,`The bot is needed more training, try to say "thanks a lot" or "hi" to the bot` );
+    }else{
+       if(entityChosen === "wit$greetings"){
+           //send greetings message
+           callSendAPI(sender_psid,'Hi there! This bot is created by Hary Pham. Watch more videos on HaryPhamDev Channel!');
+       }
+       if(entityChosen === "wit$thanks"){
+           //send thanks message
+           callSendAPI(sender_psid,`You 're welcome!`);
+       }
+        if(entityChosen === "wit$bye"){
+            //send bye message
+            callSendAPI(sender_psid,'bye-bye!');
+        }
+    }
   
   }
 
@@ -123,8 +110,87 @@ app.get('/webhook', (req, res) => {
    
     console.log('sender_psid:', recipientId);
     console.log('payload:', payload);
+
+    let response;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": { "text": response }
+    };
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v18.0/me/messages",
+        "qs": { "access_token": process.env.FB_PAGE_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!');
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
+let callSendAPIWithTemplate = (sender_psid) => {
+    // document fb message template
+    // https://developers.facebook.com/docs/messenger-platform/send-messages/templates
+    let body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [
+                        {
+                            "title": "Want to build sth awesome?",
+                            "image_url": "https://www.nexmo.com/wp-content/uploads/2018/10/build-bot-messages-api-768x384.png",
+                            "subtitle": "Watch more videos on my youtube channel ^^",
+                            "buttons": [
+                                {
+                                    "type": "web_url",
+                                    "url": "https://bit.ly/subscribe-haryphamdev",
+                                    "title": "Watch now"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    };
+
+    request({
+        "uri": "https://graph.facebook.com/v6.0/me/messages",
+        "qs": { "access_token": process.env.FB_PAGE_TOKEN },
+        "method": "POST",
+        "json": body
+    }, (err, res, body) => {
+        if (!err) {
+            // console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+};
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
