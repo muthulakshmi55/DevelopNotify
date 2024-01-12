af57b900eb9a99692191d9cfdf3e0bb3b019be57
MuthuLakshmi <muthulakshmi@zoifintech.com>
2024-01-12 19:02:02 +0530

    error resolved

--- routes/Server.js
+++ routes/Server.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config({path:'../.env'})
 const facebook = require('fb-messenger-bot-api');
 const messagingClient = new facebook.FacebookMessagingAPIClient("EAAWBW0YnBP0BO2NZCnytVVTOQypZBt7dqmNwlWwSdPWiEb3ZAw5tH0Ku0k9nHippHqLO0EPQyJGogZCl4EjNMosZBNfxui7Dy3Ql77Ud3lhd4pXaBt3BxxL3uZBdVrZC4l07iDp7E5Ld2TNpbiSeV6wsmDHs1SLOtfNwF4L0rGxuacZAivXJsFWu3dDWG7tnDtDx");
 const messageParser = facebook.FacebookMessageParser;
@@ -12,26 +14,27 @@ app.get('/', (req, res) => {
       res.send('Hello, World!');
   });
   
-  app.get('/webhook', (req, res) => {
-    let body = req.body;
-      if (req.method === 'GET') {
-          let VERIFY_TOKEN = process.env.VERIFY_FB_TOKEN
+//   app.get('/webhook', (req, res) => {
+//     let body = req.body;
+//       if (req.method === 'GET') {
+//           let VERIFY_TOKEN = process.env.VERIFY_FB_TOKEN
   
-          let mode = req.query['hub.mode'];
-          let token = req.query['hub.verify_token'];
-          let challenge = req.query['hub.challenge'];
+//           let mode = req.query['hub.mode'];
+//           let token = req.query['hub.verify_token'];
+//           let challenge = req.query['hub.challenge'];
   
-          if (mode && token === VERIFY_TOKEN) {
-              res.status(200).send(challenge);
-          } else {
-              res.sendStatus(403);
-          }
-          res.status(200).send("CHALLENGE_ACCEPTED");
-      } else {
-          res.sendStatus(405); // Method Not Allowed
-      }
-  });
+//           if (mode && token === VERIFY_TOKEN) {
+//               res.status(200).send(challenge);
+//           } else {
+//               res.sendStatus(403);
+//           }
+//           res.status(200).send("CHALLENGE_ACCEPTED");
+//       } else {
+//           res.sendStatus(405); // Method Not Allowed
+//       }
+//   });
 app.post('/webhook', (req, res) => {
+    console.log("verify",process.env.VERIFY_FB_TOKEN)
     // Log the incoming payload for debugging
     console.log('Incoming payload:', JSON.stringify(req.body, null, 2));
 
